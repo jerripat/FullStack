0e43d252a5e10ff35e4c61925faa1088dac798a2
jerripat <jerripatterson@gmail.com>
2025-12-26 20:31:04 -0600

    changes to the About section

--- FullStack/settings.py
+++ FullStack/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 
 ALLOWED_HOSTS = ["*"]
 

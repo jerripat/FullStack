764959f639660b6bb754ca2ad2fc026e3c5b92c7
jerripat <jerripatterson@gmail.com>
2026-01-01 13:00:27 -0600

    ADDED new contact form and contact css

--- FullStack/settings.py
+++ FullStack/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY", default="django-insecure-dev-only")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = True #config("DEBUG", default=False, cast=bool)
 
 
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "full_stack", "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

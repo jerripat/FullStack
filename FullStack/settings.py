8b031b9adb3d7d606d82e26db57318d5bac32178
jerripat <jerripatterson@gmail.com>
2026-01-01 06:03:22 -0600

    settings changes

--- FullStack/settings.py
+++ FullStack/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = 'jfgudd755jdjjjdlsa009kkxj15v84k9t8c5j'
+SECRET_KEY = config('SECRET_KEY')
+#SECRET_KEY = 'jfgudd755jdjjjdlsa009kkxj15v84k9t8c5j'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
